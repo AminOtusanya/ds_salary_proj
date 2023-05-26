382553bfbf964a475b9bd72d44af5aeaa0cb5182
Amin Otusanya <alaminotusanya@gmail.com>
2023-05-26 07:36:17 -0400

    Uploaded all initial data

---
+++ data collection.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+df=pd.read_csv("C:/Users/otusa/OneDrive/Documents/ds_salary_proj/Uncleaned_DS_jobs.csv/Uncleaned_DS_jobs.csv")
---
+++ ds_ salary_code.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 19:23:51 2023
+
+@author: otusa
+"""
+
+import pandas as pd 
+import numpy as np 
+import matplotlib as plt 
+
+df = pd.read_csv("C:\Users\otusa\OneDrive\Documents\ds_salary_proj\Uncleaned_DS_jobs.csv\Uncleaned_DS_jobs.csv")
